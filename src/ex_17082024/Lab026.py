939aec8a6cba6576b08dd637394346c57c5eafc4
Parvathivanga <parvathi.chemical@gmail.com>
2024-08-21 11:53:30 +0530

    Tasks are added

--- src/ex_17082024/Lab026.py
+++ src/ex_17082024/Lab026.py
@@ -1,1 +1,17 @@
-##
+#name = input("Enter your name")
+#print(name)
+
+#variable
+# it is a container that stores
+#variable_name=variable_value
+age=65
+print(age)
+age=age+1
+print(age)
+age+2
+print(age)
+
+#Datatypes
+#int,float,str,bool,complex-Basic
+#list,tupl, dict,set,frozenset,nonetype-Advance
+#Pythonordered dict
